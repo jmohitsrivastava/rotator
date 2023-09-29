24fb507b5e5ccf7ddf75f14d859919a9af39e181
jmohitsrivastava <144433083+jmohitsrivastava@users.noreply.github.com>
2023-09-30 01:31:53 +0530

    first rotator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  let [animate,setanimate]=useState("ball");
+  let[btnname,setbtnname]=useState("start")
+  let [chek,setchek]=useState(false)
+
+  const rotate=()=>{
+    
+    if(chek===false){
+    const csss=document.querySelector("#bal").className;
+    setanimate("ball-rotate")
+    setbtnname("stop")
+    setchek(true)
+  }else{
+    setanimate("ball")
+    setbtnname("start")
+    setchek(false)
+  }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <>
+     <div className="parent-div">
+
+      {/* /button */}
+      <div  className="button-box">
+      <button id="btn-1" onClick={rotate}>{btnname}</button>
+       </div>
+     {/* /button end */}
+
+
+     {/* ball's */}
+          
+   <div className='balls'>
+
+       {/* ball 1*/}    
+        <div id="bal" className={animate} >
+       
+        </div>
+
+        {/* ball 2*/}    
+        <div  style={{marginTop:"22vh"}} id="bal" className={animate} >
+       
+        </div>
+
+        {/* ball 2*/}    
+        <div  style={{marginTop:"44vh"}} id="bal" className={animate} >
+       
+        </div>
+
+        {/* ball 3*/}    
+        <div  style={{marginTop:"65vh"}} id="bal" className={animate} >
+       
+        </div>
+
+  
+ </div>    
+    
     </div>
+    </>
   );
 }
 
